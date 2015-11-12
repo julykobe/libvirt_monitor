257aad4115c0cea8ab4dd44442d90dcb5d28adf1
julykobe <myzswen@gmail.com>
2015-11-12 15:02:11 +0800

    unified format

--- getNodeInfo.py
+++ getNodeInfo.py
@@ -2,7 +2,7 @@ import libvirt
 import os,sys,re,time
 from xml.etree import ElementTree
 
-def get_devices(dom,path,devs):
+def getDevices(dom,path,devs):
 	tree=ElementTree.fromstring(dom.XMLDesc(0))
 	devices=[]
 	for target in tree.findall(path):
@@ -11,81 +11,81 @@ def get_devices(dom,path,devs):
 			devices.append(dev)
 	return devices
 
-def get_blockStats(dom):
-	block_status={}
-	disks=get_devices(dom,"devices/disk/target","dev")
+def getBlockStats(dom):
+	blockStatus={}
+	disks=getDevices(dom,"devices/disk/target","dev")
 	for block in disks:
-		block_status[block]=dom.blockStats(block)
-	return block_status
+		blockStatus[block]=dom.blockStats(block)
+	return blockStatus
 
-def get_domDiskInfo(dom0):
-	block_status0={}
-	block_status1={}
-	disk_info=''
-	block_status0=get_blockStats(dom0)
+def getDomDiskInfo(dom0):
+	blockStatus0={}
+	blockStatus1={}
+	diskInfo=''
+	blockStatus0=getBlockStats(dom0)
 	time.sleep(2)
-	block_status1=get_blockStats(dom0)
-	block_info=[]
-	for block in get_devices(dom0,"devices/disk/source","file"):
-		block_info.append(dom0.blockInfo(block,0))
+	blockStatus1=getBlockStats(dom0)
+	blockInfo=[]
+	for block in getDevices(dom0,"devices/disk/source","file"):
+		blockInfo.append(dom0.blockInfo(block,0))
 	i=0
-	for block in get_devices(dom0,"devices/disk/target","dev"):
-		disk_info+=block+'-'+str((block_status1[block][1]-block_status0[block][1])/2048)+'-'+ str((block_status1[block][3]-block_status0[block][3])/2048)+'-'+str(block_info[i][1]/1.0/block_info[i][0]*100)[:5]+'-'+str(block_info[i][0])+'#'
+	for block in getDevices(dom0,"devices/disk/target","dev"):
+		diskInfo+=block+'-'+str((blockStatus1[block][1]-blockStatus0[block][1])/2048)+'-'+ str((blockStatus1[block][3]-blockStatus0[block][3])/2048)+'-'+str(blockInfo[i][1]/1.0/blockInfo[i][0]*100)[:5]+'-'+str(blockInfo[i][0])+'#'
 		i=i+1
-	return disk_info
+	return diskInfo
 
-def get_domCPUUsage(dom0):
-	Pstart_time=time.time()
-	Dstart_time=dom0.info()[4]
+def getDomCpuUsage(dom0):
+	PstartTime=time.time()
+	DstartTime=dom0.info()[4]
 	time.sleep(2)
-	Dstop_time=dom0.info()[4]
-	Pstop_time=time.time()
-	core_num=int(dom0.info()[3])
-	cpu_usage=(Dstart_time-Dstop_time)/(Pstart_time-Pstop_time)/1000000000/core_num*100
-	cpu_usage=cpu_usage if (cpu_usage>0) else 0.0
-	cpu_usage=cpu_usage if (cpu_usage<100) else 100.0
-	return cpu_usage
+	DstopTime=dom0.info()[4]
+	PstopTime=time.time()
+	coreNum=int(dom0.info()[3])
+	cpuUsage=(DstartTime-DstopTime)/(PstartTime-PstopTime)/1000000000/coreNum*100
+	cpuUsage=cpuUsage if (cpuUsage>0) else 0.0
+	cpuUsage=cpuUsage if (cpuUsage<100) else 100.0
+	return cpuUsage
 
-def get_memory(pid):
+def getMemory(pid):
 	mem=0
 	for line in file('/proc/%d/smaps' % int(pid),'r'):
 		if re.findall('Private_',line):
 			mem+=int(re.findall('(\d+)',line)[0])
 	return mem
 
-def get_domMemUsage(dom0):
+def getDomMemUsage(dom0):
 	pid=(os.popen("ps aux|grep "+dom0.name()+" | grep -v 'grep' | awk '{print $2}'").readlines()[0])
-	memstatus=get_memory(pid)
-	memusage='%.2f' % (int(memstatus)*100.0/int(dom0.info()[2]))
-	return memusage
+	memStatus=getMemory(pid)
+	memUsage='%.2f' % (int(memStatus)*100.0/int(dom0.info()[2]))
+	return memUsage
 
-def get_nicInfo(nics):
-	net_status={}
+def getNicInfo(nics):
+	netStatus={}
 	for nic in nics:
-		net_status[nic]=[os.popen("cat /proc/net/dev |grep -w '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep -w '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
-	return net_status
+		netStatus[nic]=[os.popen("cat /proc/net/dev |grep -w '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep -w '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
+	return netStatus
 
-def get_domNicInfo(dom0):
-	net_status0={}
-	net_status1={}
-	net_info=''
-	nics=get_devices(dom0,"devices/interface/target","dev")
-	net_status0=get_nicInfo(nics)
+def getDomNicInfo(dom0):
+	netStatus0={}
+	netStatus1={}
+	netInfo=''
+	nics=getDevices(dom0,"devices/interface/target","dev")
+	netStatus0=getNicInfo(nics)
 	time.sleep(2)
-	net_status1=get_nicInfo(nics)
+	netStatus1=getNicInfo(nics)
 	for nic in nics:
-		net_info+=nic+'-'+str((int(net_status1[nic][0])-int(net_status0[nic][0]))/2048)+'-'+str((int(net_status1[nic][1])-int(net_status0[nic][1]))/2048)+'#'
-	return net_info
+		netInfo+=nic+'-'+str((int(netStatus1[nic][0])-int(netStatus0[nic][0]))/2048)+'-'+str((int(netStatus1[nic][1])-int(netStatus0[nic][1]))/2048)+'#'
+	return netInfo
 
-def get_domIPInfo(dom0):
-	domMain_info=[]
-	#instance_ip=ElementTree.fromstring(dom0.XMLDesc(0)).findall("devices/interface/filterref/parameter")[1].get('value')
-	instance_ip = ''
-	domMain_info.append(dom0.name())
+def getDomIpInfo(dom0):
+	domMainInfo=[]
+	#instanceIp=ElementTree.fromstring(dom0.XMLDesc(0)).findall("devices/interface/filterref/parameter")[1].get('value')
+	instanceIp = ''
+	domMainInfo.append(dom0.name())
 	domUUID=ElementTree.fromstring(dom0.XMLDesc(0)).findtext('uuid')
-	domMain_info.append(domUUID)
-	domMain_info.append(instance_ip)
-	return domMain_info
+	domMainInfo.append(domUUID)
+	domMainInfo.append(instanceIp)
+	return domMainInfo
 
 def getNodeInfo(domainID):
 	conn=libvirt.open("qemu:///system")
@@ -98,11 +98,11 @@ def getNodeInfo(domainID):
 	except:
 		print "fail to find the domain"
 		sys.exit(1)
-	node_info=[]
-	node_info.append(get_domCPUUsage(dom0))
-	node_info.append(get_domMemUsage(dom0))
-	node_info.append(get_domDiskInfo(dom0))
-	node_info.append(get_domNicInfo(dom0))
-	for info in get_domIPInfo(dom0):
-		node_info.append(info)
-	return node_info
+	nodeInfo=[]
+	nodeInfo.append(getDomCpuUsage(dom0))
+	nodeInfo.append(getDomMemUsage(dom0))
+	nodeInfo.append(getDomDiskInfo(dom0))
+	nodeInfo.append(getDomNicInfo(dom0))
+	for info in getDomIpInfo(dom0):
+		nodeInfo.append(info)
+	return nodeInfo
